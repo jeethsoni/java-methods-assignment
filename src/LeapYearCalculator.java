618c09782ee2032c0f1421afd3f0bde83e928786
Jeet Soni <jeet.soni0822@gmail.com>
2024-12-07 11:26:53 -0500

    Leap year calculator

---
+++ src/LeapYearCalculator.java
@@ -0,0 +1,19 @@
+public class LeapYearCalculator {
+    public static boolean isLeapYear(int year){
+        if (year <= 0){
+            return false;
+        }
+
+        if (year % 4 == 0){
+            if (year % 100 == 0){
+                if (year % 400 == 0){
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import
-
 public class Main {
     public static void main(String[] args) {
         PositiveNegativeZero.checkNumber(5);
         SpeedConverter.printConversion(-1);
+        boolean isLeap = LeapYearCalculator.isLeapYear(-4000);
+        System.out.println(isLeap);
     }
 }
